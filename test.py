9fef1db62f640306aaaad8902daedc3265d71662
EnderDuck247 <nucleargamer1103@gmail.com>
2021-01-23 16:32:46 +0530

    Revert "Revert "Merge branch 'master' of https://github.com/EZCLAP03/COVID19App-Codefest-2021""
    This reverts commit de402542549b44b9fd57d42368cbeb8eb560b05e.

---
+++ test.py
@@ -0,0 +1,30 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRectangleFlatButton
+from kivy.lang import Builder
+import input
+from main import covid19api
+
+
+class CovidApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = "Dark"
+        screen = Screen()
+        self.country = Builder.load_string(input.country)
+        button = MDRectangleFlatButton(text='Show',
+                                       pos_hint={'center_x': 0.5, 'center_y': 0.3},
+                                       on_release=self.show_data)
+        screen.add_widget(self.country)
+        screen.add_widget(button)
+        return screen
+
+    def show_data(self, obj):
+        data = covid19api()
+        print(self.country.text)
+        for content in data['Country']:
+            if content['name'] == self.country.text:
+                print(content)
+
+
+CovidApp().run()
