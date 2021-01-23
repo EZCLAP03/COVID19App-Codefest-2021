9e8cb521faea327747c4aa82cf448e0c1df0aa1c
Siddysensation <46835739+Siddysensation@users.noreply.github.com>
2021-01-23 11:46:22 +0530

    Base of navigation drawer
    Aesthectics of navigation drawer

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
+from kivy.lang import Builder
+
+navigation_bar = """
+Screen:
+    BoxLayout: 
+        orientation: 'vertical'
+        MDToolbar: 
+            title: 'Covid App'
+        MDLabel:
+            text: 'Cases near you'
+            halign: 'center'
+            
+"""
 
 
-class CovidApp(MDApp)
+class CovidApp(MDApp):
 
-        def build(self):
-            screen = Screen()
-            return screen
+    def build(self):
+        screen = Builder.load_string(navigation_bar)
+        return screen
+
+
+CovidApp().run()
