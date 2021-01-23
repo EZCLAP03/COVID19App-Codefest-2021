ce187a39a1dc8b92e82f48a7e95b3436c5d84b09
Siddysensation <46835739+Siddysensation@users.noreply.github.com>
2021-01-23 11:21:13 +0530

    Default layout for kivy
    Default layout of app

--- app.py
+++ app.py
@@ -3,6 +3,30 @@ from kivy.app import App
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.button import Button
 
-    class PageLayout(PageLayout):
 
-            
+class Layout(PageLayout):
+
+    def __init__(self):
+        super(PageLayout, self).__init__()
+
+        btn1 = Button(text='Page 1')
+
+        btn2 = Button(text='Page 2')
+
+        btn3 = Button(text='Page 3')
+
+        self.add_widget(btn1)
+
+        self.add_widget(btn2)
+
+        self.add_widget(btn3)
+
+
+class LayoutApp(App):
+
+    def build(self):
+        return PageLayout()
+
+
+if __name__ == '__main__':
+    LayoutApp().run()
