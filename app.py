489093fc3f87dd4c8fccef81dcdf3221bd25ab41
EnderDuck247 <nucleargamer1103@gmail.com>
2021-01-23 14:56:09 +0530

    1sfhlasjdswlkfjh

--- app.py
+++ app.py
@@ -1,49 +1,6 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-
-toolbar_helper = """
-Screen:
-    MDTextFieldRound:
-        size_hint_x: None
-        width: "100dp"
-        id: Arrival
-        hint_text: "Departure" 
-        pos_hint: {"center_x": 0.75, "center_y": 0.8}
-
-    MDTextFieldRound:
-        size_hint_x: None 
-        width: "100dp"
-        id: Arrival
-        hint_text: "Arrival" 
-        pos_hint: {"center_x": 0.25, "center_y": 0.8}       
-        
-    NavigationLayout:
-        ScreenManager:
-            Screen:
-                BoxLayout:
-                    orientation: 'vertical'
-                    MDToolbar:
-                        title: "Covid App"
-                        elevation: 10
-                        left_action_items: [['menu', lambda x: nav_drawer.set_state()]]
-                    Widget:
-        MDNavigationDrawer:
-            id: nav_drawer
-            
-            BoxLayout:
-                orientation: 'vertical'
-                
-                MDLabel:
-                    text: 'Username'
-                    font_style: 'Caption'
-                MDLabel:
-                    text: 'Location'
-                    font_style: 'Caption'         
-        
-                    
-                    
-"""
-
+from helpers import toolbar_helper
 
 class CovidApp(MDApp):
 
