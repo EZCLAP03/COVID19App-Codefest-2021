d9d96c5800860a1d26e0085e4aeac5bfad1752ae
Siddysensation <46835739+Siddysensation@users.noreply.github.com>
2021-01-23 15:33:28 +0530

    Input has been achieved

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
-import input
+from kivymd.uix.button import MDRectangleFlatButton
 from kivy.lang import Builder
+import input
+
 
 class CovidApp(MDApp):
-
-class DemoApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette="Blue"
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = "Dark"
         screen = Screen()
-
-
-        country = Builder.load_string(input.country)
-        screen.add_widget(country)
+        self.country = Builder.load_string(input.country)
+        button = MDRectangleFlatButton(text='Show',
+                                       pos_hint={'center_x': 0.5, 'center_y': 0.3},
+                                       on_release=self.show_data)
+        screen.add_widget(self.country)
+        screen.add_widget(button)
         return screen
+
+    def show_data(self, obj):
+        print(self.country.text)
 
 
 CovidApp().run()
-DemoApp().run()
