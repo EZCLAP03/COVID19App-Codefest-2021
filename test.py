a21d5817414a102b7d6525a36ae972e7c1a318ee
Siddysensation <46835739+Siddysensation@users.noreply.github.com>
2021-01-23 18:15:22 +0530

    Test,py has full functionality now

--- test.py
+++ test.py
@@ -10,23 +10,24 @@ from main import covid19api
 class CovidApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Blue"
-        self.theme_cls.theme_style = "Dark"
         screen = Screen()
-        self.user_input = Builder.load_string(input.user)
+        self.theme_cls.theme_style = "Dark"
+        self.user = Builder.load_string(input.user)
         button = MDRectangleFlatButton(text='Show',
                                        pos_hint={'center_x': 0.5, 'center_y': 0.3},
                                        on_release=self.show_data)
-        screen.add_widget(self.user_input)
+        screen.add_widget(self.user)
         screen.add_widget(button)
         return screen
 
     def show_data(self, obj):
+        print(self.user.text)
         data = covid19api()
         for content in data['Country']:
-            if content['name'] == self.user_input.text:
-                dialog = MDDialog(title='Covid 19 stats in your preferred area', text=self.user_input.text)
+            if content['name'] == self.user.text:
+                dialog = MDDialog(title='Covid 19 cases in your preferred area', text=content["Total_cases"])
                 dialog.open()
-                print(content)
+                print(content["Total_cases"])
 
 
 CovidApp().run()
