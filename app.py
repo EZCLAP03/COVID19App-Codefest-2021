38b81cc44cb6daab94317b2fa19da4fc79043699
harry <harisaikarthikeyan@gmail.com>
2021-01-23 15:43:30 +0530

    mn

--- app.py
+++ app.py
@@ -1,25 +1,56 @@
 from kivymd.app import MDApp
-from kivymd.uix.screen import Screen
-from kivymd.uix.button import MDRectangleFlatButton
 from kivy.lang import Builder
-import input
+from main import covid19stats
+from kivymd.uix.label import MDLabel
+
+toolbar_helper = """
+Screen:
+
+    MDTextFieldRound:
+        size_hint_x: None
+        width: "100dp"
+        id: Arrival
+        hint_text: "Departure" 
+        pos_hint: {"center_x": 0.75, "center_y": 0.8}
+
+    MDTextFieldRound:
+        size_hint_x: None 
+        width: "100dp"
+        id: Arrival
+        hint_text: "Arrival" 
+        pos_hint: {"center_x": 0.25, "center_y": 0.8}       
+
+    NavigationLayout:
+        ScreenManager:
+            Screen:
+                BoxLayout:
+                    orientation: 'vertical'
+                    MDToolbar:
+                        title: "Covid App"
+                        elevation: 10
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state()]]
+                    Widget:
+        MDNavigationDrawer:
+            id: nav_drawer
+            
+            BoxLayout:
+                orientation: 'vertical'
+                
+                MDLabel:
+                    text: 'Username'
+                    font_style: 'Caption'
+                MDLabel:
+                    text: 'Location'
+                    font_style: 'Caption'                         
+"""
 
 
 class CovidApp(MDApp):
+
     def build(self):
-        self.theme_cls.primary_palette = "Blue"
-        self.theme_cls.theme_style = "Dark"
-        screen = Screen()
-        self.country = Builder.load_string(input.country)
-        button = MDRectangleFlatButton(text='Show',
-                                       pos_hint={'center_x': 0.5, 'center_y': 0.3},
-                                       on_release=self.show_data)
-        screen.add_widget(self.country)
-        screen.add_widget(button)
+        screen = Builder.load_string(toolbar_helper)
         return screen
 
-    def show_data(self, obj):
-        print(self.country.text)
-
-
 CovidApp().run()
+
+
---
+++ test.py
@@ -0,0 +1,26 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRectangleFlatButton
+from kivy.lang import Builder
+import input
+from main import 
+
+
+class CovidApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = "Dark"
+        screen = Screen()
+        self.country = Builder.load_string(input.country)
+        button = MDRectangleFlatButton(text='Show',
+                                       pos_hint={'center_x': 0.5, 'center_y': 0.3},
+                                       on_release=self.show_data)
+        screen.add_widget(self.country)
+        screen.add_widget(button)
+        return screen
+
+    def show_data(self, obj):
+        print(self.country.text)
+
+
+CovidApp().run()
