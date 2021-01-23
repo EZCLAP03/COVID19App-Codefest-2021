a6bbadde1b8104fe8ade346f0c108349ab8a4279
Siddysensation <46835739+Siddysensation@users.noreply.github.com>
2021-01-23 15:03:59 +0530

    Final Layout , yes we have decided

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+import input
 from kivy.lang import Builder
-from helpers import toolbar_helper
 
 class CovidApp(MDApp):
 
+class DemoApp(MDApp):
     def build(self):
-        screen = Builder.load_string(toolbar_helper)
+        self.theme_cls.primary_palette="Blue"
+        screen = Screen()
+
+
+        country = Builder.load_string(input.country)
+        screen.add_widget(country)
         return screen
 
 
 CovidApp().run()
+DemoApp().run()
---
+++ input.py
@@ -0,0 +1,11 @@
+country = """
+MDTextField:
+    hint_text: "Enter Your Destination"
+    helper_text: "or your area"
+    helper_text_mode: "on_focus"
+    icon_right: "menu"
+    icon_right_color: app.theme_cls.primary_color
+    pos_hint:{'center_x': 0.5, 'center_y': 0.5}
+    size_hint_x:None
+    width:300
+"""
