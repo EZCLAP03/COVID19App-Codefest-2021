6a1a3397d6da009e3ed038ed3685a9d01e1ea8ec
harry <harisaikarthikeyan@gmail.com>
2021-01-24 03:06:19 +0530

    added screen change

--- nav.py
+++ nav.py
@@ -24,10 +24,20 @@ Screen:
 
             Screen:
                 name: "scr 1"
-
-                MDLabel:
-                    text: "Covid"
-                    halign: "center"
+                MDTextField:
+                    hint_text: "Enter Your Destination"
+                    helper_text: "or your area"
+                    helper_text_mode: "on_focus"
+                    icon_right: "menu"
+                    icon_right_color: app.theme_cls.primary_color
+                    pos_hint:{'center_x': 0.5, 'center_y': 0.5}
+                    size_hint_x:None
+                    width:300
+                
+                MDRectangleFlatButton: 
+                    text: 'Show'
+                    pos_hint: {'center_x': 0.5, 'center_y': 0.3}
+                    on_release: self.show_data
 
             Screen:
                 name: "scr 2"
