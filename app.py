151724a1f6539592e8ef39d4c349520e397b8f9b
EnderDuck247 <nucleargamer1103@gmail.com>
2021-01-23 16:30:31 +0530

    pop up

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
+from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDRectangleFlatButton
 from kivy.lang import Builder
 import input
@@ -8,8 +9,8 @@ import input
 class CovidApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Blue"
-        self.theme_cls.theme_style = "Dark"
         screen = Screen()
+        self.theme_cls.theme_style = "Dark"
         self.country = Builder.load_string(input.country)
         button = MDRectangleFlatButton(text='Show',
                                        pos_hint={'center_x': 0.5, 'center_y': 0.3},
@@ -20,6 +21,14 @@ class CovidApp(MDApp):
 
     def show_data(self, obj):
         print(self.country.text)
+<<<<<<< Updated upstream
+=======
+        for content in data['Country']:
+            if content['name'] == self.country.text:
+                dialog = MDDialog(text=content["Total_cases"])
+                dialog.open()
+                print(content["Total_cases"])
+>>>>>>> Stashed changes
 
 
 CovidApp().run()
